b8cdc4928dc22abbbfbc4ef2f488bc06285a64d1
Borys Kopeć <boryskopec@proton.me>
2026-03-18 21:35:33 +0100

    1 solved

---
+++ src/bin/1.rs
@@ -0,0 +1,14 @@
+// <p>If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, we get $3, 5, 6$ and $9$. The sum of these multiples is $23$.</p>
+// <p>Find the sum of all the multiples of $3$ or $5$ below $1000$.</p>
+
+fn main() {
+    let mut answer = 0;
+    // Naive loop
+    for n in 1..1000 {
+        if n % 3 == 0 || n % 5 == 0 {
+            answer += n;
+        }
+    }
+
+    println!("{answer}");
+}
